1f2cf8c25e94bca005cfa47c33a36541d140834b
Brenley Dueck <brenleydueck@gmail.com>
2023-02-15 18:40:15 -0600

    Initial scaffold

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import React from 'react';
+import { useEffect, useRef, useState } from 'react';
+
+function App() {
+
+    return (
+        <div />
+    )
+}
+
+export default App;
